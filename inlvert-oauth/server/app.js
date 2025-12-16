54a6a094daafde7db847d875c43708158f4ce058
Boris Ushakov <inlvert@gmail.com>
2025-12-16 15:05:35 +0200

    add changes to app

--- inlvert-oauth/server/app.js
+++ inlvert-oauth/server/app.js
@@ -1,6 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const cors = require("cors");
+const router = require("./routers");
+const session = require("express-session");
 
-// Middleware and route definitions would go here
+require("./config/passport");
+const passport = require("passport");
+
+
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+app.use(
+  session({
+    secret: "your_secret_key",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(router);
 
 module.exports = app;
