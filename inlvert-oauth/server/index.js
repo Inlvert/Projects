f6d5e525678864ee1439713bd788902720cb5fd0
Boris Ushakov <inlvert@gmail.com>
2025-12-16 15:04:23 +0200

    add changes

--- inlvert-oauth/server/index.js
+++ inlvert-oauth/server/index.js
@@ -1,10 +1,23 @@
-const http = require('http');
-const app = require('./app');
+const http = require("http");
+const app = require("./app");
+const CONSTANTS = require("./constants");
+const {connectToDB} = require("./models/index");
 
-const PORT = process.env.PORT || 3000;
+const PORT = CONSTANTS.PORT || 5000;
 
-const server = http.createServer(app);
+async function startServer() {
+  try {
+    await connectToDB();
 
-server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+    const server = http.createServer(app);
+    server.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+
+  } catch (error) {
+    console.error("Failed to start server", error);
+    process.exit(1);
+  }
+}
+
+startServer();
